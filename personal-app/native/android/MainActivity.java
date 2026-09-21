ff34a42949971f446bc3de02f852703907deb349
needis123-arch <needis123@gmail.com>
2026-09-21 15:10:46 +0900

    Fix TodayBoard registration before Capacitor bridge creation

--- personal-app/native/android/MainActivity.java
+++ personal-app/native/android/MainActivity.java
@@ -6,7 +6,7 @@ import com.getcapacitor.BridgeActivity;
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         registerPlugin(TodayBoardPlugin.class);
+        super.onCreate(savedInstanceState);
     }
 }

48d2271e7338fd483ea6faa89863bd8eb02f5bd5
needis123-arch <needis123@gmail.com>
2026-09-21 12:44:49 +0900

    Add native Android today board notification

---
+++ personal-app/native/android/TodayBoardReceiver.java
@@ -0,0 +1,19 @@
+package kr.co.hirealty.personalcenter;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class TodayBoardReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent != null ? intent.getAction() : "";
+        if (TodayBoardManager.ACTION_COMPLETE.equals(action)) {
+            TodayBoardManager.markComplete(context, intent);
+        } else if (TodayBoardManager.ACTION_REMIND.equals(action)) {
+            TodayBoardManager.showBoard(context, true);
+        } else if (TodayBoardManager.ACTION_REFRESH.equals(action)) {
+            TodayBoardManager.showBoard(context, false);
+        }
+    }
+}
